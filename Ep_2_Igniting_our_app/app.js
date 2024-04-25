be99b84082e9e7fe8314ad087143a7a6da2c57b1
monukdjavadev <monukd01dev@gmail.com>
2024-04-26 00:56:58 +0530

    ep2 done with code

---
+++ Ep_2_Igniting_our_app/app.js
@@ -0,0 +1,11 @@
+ import React from 'react';
+ import ReactDOM from 'react-dom/client';
+
+//   stablish connection b/w React and HTML
+const ROOT = ReactDOM.createRoot(document.getElementById('root'));
+
+// now we create our element
+const responsiveImage = React.createElement('img',{src : "https://source.unsplash.com/random", width : '100%'});
+
+// Finalising using render
+ROOT.render(responsiveImage)
