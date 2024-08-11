2e54523479e7e5ee596d1116def8ac42b6c64b22
monukdjavadev <monukd01dev@gmail.com>
2024-08-11 22:33:59 +0530

    hook note making

--- Ep_4_Talk_is_cheap_show_the_code/FlavorFusion/src/components/Main.js
+++ Ep_4_Talk_is_cheap_show_the_code/FlavorFusion/src/components/Main.js
@@ -2,17 +2,36 @@ import Search from "./Search";
 import RestaurantCard from "./RestaurantCard";
 import restaurantsList from "../../utils/restaurantsMockData.json";
 
-const Main = () => (
-	<div className="main">
-		<div className="search-con">
-			<Search />
+const Main = () => {
+	let resList = [...restaurantsList];
+	// restaurantsList = [];it's readonly
+	return (
+		<div className="main">
+			<div className="search-con">
+				<Search />
+			</div>
+			<div className="filters">
+				<button
+					type="button"
+					className="static-btn"
+					onClick={() => {
+						console.log("-----button clicked--------");
+						console.log("before : ", resList);
+						resList = [];
+						console.log("After : ", resList);
+						console.log("----- End --------\n");
+					}}
+				>
+					Top Rated
+				</button>
+			</div>
+			<div className="restaurant-container">
+				{resList.map((data) => (
+					<RestaurantCard key={data?.info?.id} {...data?.info} />
+				))}
+			</div>
 		</div>
-		<div className="restaurant-container">
-			{restaurantsList.map((data) => (
-				<RestaurantCard key={data?.info?.id} {...data?.info} />
-			))}
-		</div>
-	</div>
-);
+	);
+};
 
 export default Main;
