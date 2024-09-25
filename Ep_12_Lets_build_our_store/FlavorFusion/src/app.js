b3c003e4eda8ae165eb9551000f21b182ca94017
monukdjavadev <monukd01dev@gmail.com>
2024-09-25 21:46:56 +0530

    removed contextapi code and pushed to production

--- Ep_12_Lets_build_our_store/FlavorFusion/src/app.js
+++ Ep_12_Lets_build_our_store/FlavorFusion/src/app.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense, useState } from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 //importing components
 import Header from "./components/Header";
@@ -7,40 +7,22 @@ import ResMenuShimmer from "./components/ResMenuShimmer";
 //pages
 import Contact from "./components/Contact";
 import MyError from "./components/MyError";
-// import RestaurantMenu from "./components/RestaurantMenu";
-const RestaurantMenu = lazy(() => import("./components/RestaurantMenu"));
-
-// * making About lazy-loading, dynamic import
-// import About from "./components/About";
-// ? 1. lazy(()=> import("./components/About"))
-// ? 2. <Suspence fallback={/* yaha simmer dete hai */}></Suspence>
-
-// ! let's see the code
-const About = lazy(() => import("./components/About"));
 
 //react-rounter code
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
-import UserContext from "../utils/UserContext";
 
+// ! Lazy Loading
+const RestaurantMenu = lazy(() => import("./components/RestaurantMenu"));
+const About = lazy(() => import("./components/About"));
+
+// creating root
 const Root = ReactDOM.createRoot(document.getElementById("root"));
 
 const AppLayout = () => {
-	const [mainName, setMainName] = useState("mainUser");
-
 	return (
 		<div className="app">
-			<UserContext.Provider
-				value={{
-					userName: mainName,
-					setMainName: setMainName,
-					userEmail: "monukd01dev@gmail.com",
-					userAge: 18,
-					isPro: true,
-				}}
-			>
-				<Header />
-				<Outlet />
-			</UserContext.Provider>
+			<Header />
+			<Outlet />
 		</div>
 	);
 };
--- Ep_12_Lets_build_our_store/FlavorFusion/src/components/Header.js
+++ Ep_12_Lets_build_our_store/FlavorFusion/src/components/Header.js
@@ -1,9 +1,7 @@
 import { Link } from "react-router-dom";
 import { FF_LOGO } from "../../utils/constants";
-import UserContext from "../../utils/UserContext";
-import { useContext } from "react";
+
 const Header = () => {
-	const { userName } = useContext(UserContext);
 	return (
 		<div className="header">
 			<div className="logo">
@@ -13,7 +11,6 @@ const Header = () => {
 				<Link to="/">Home</Link>
 				<Link to="/contact">Contact</Link>
 				<Link to="/about">About</Link>
-				<div>{userName}</div>
 				<Link to="/cart">
 					<i className="fa-solid fa-cart-shopping" />
 				</Link>
